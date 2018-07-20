a9c27855b43c26ccd274da00bd6a5376c71cdb36
Mian Uddin <mianuddin@gmail.com>
2018-07-19 20:51:01 -0700

    ci: add linting and babel preset

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import 'airbnb-browser-shims';
 
 import reducers from './reducers';
 import SignUp from './containers/SignUp';
