9120fcb1b67d6f751a37c009c0a1d1d353d51cd0
Jummy Caraga <jumer.caragay@gmail.com>
2018-07-20 22:17:33 -0700

    feat(src): implement first draft of sign up form

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import reducers from './reducers';
+import SignUp from './containers/SignUp';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={SignUp} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.app'),
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form: formReducer,
 });
 
 export default rootReducer;
