0b32a926913202816551d32fc5bd8ba2cd9c3c5d
Jummy Caraga <jumer.caragay@gmail.com>
2018-07-20 22:17:33 -0700

    feat(src): implement first draft of sign up form

--- src/index.jsx
+++ src/index.jsx
@@ -8,16 +8,17 @@ import 'airbnb-browser-shims';
 import reducers from './reducers';
 import SignUp from './containers/SignUp';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/" component={SignUp} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </Provider>
-    , document.querySelector('.app'));
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={SignUp} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.app'),
+);
