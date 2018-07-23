cbbce3bb1af6db81d3d36b9b638ad355f93f0a1c
Jumer Ryan Caragay <31534181+Jumer-Caragay@users.noreply.github.com>
2018-07-22 21:18:41 -0700

    feat(src): implement first draft of sign up form (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
 module.exports = {
-    entry: ['./src/index.js'],
-    output: {
-      path: __dirname,
-      publicPath: '/',
-      filename: 'bundle.js'
+  entry: ['./src/index.jsx'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
     },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['babel-preset-env','react']
-            }
-          }
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    devServer: {
-      historyApiFallback: true,
-      contentBase: './',
-      watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000
-      }
-    }
-  };
-  
+  },
+};
