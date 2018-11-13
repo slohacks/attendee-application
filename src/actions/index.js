ef0f442c30ae6f5df277634337d52c434fd3a7c3
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2018-11-13 11:52:42 -0800

    feat(src/, actions/, components/): implement user signup with email verification (#22)
    * feat(./src): implement user signup with email verification
    
    * fix(containers/SignUp.jsx): remove unnecessary import statement
    
    Co-Authored-By: stonewallstan <stanleyarmstrong31@gmail.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
 import * as types from './types';
 import { firebase, applicationsRef } from '../config/firebase';
 
-export function signUp(values) {
-  return {
-    type: types.SIGN_UP_GUCCI,
-    payload: values,
-  };
-}
+export const signUp = values => (dispatch) => {
+  firebase.auth().createUserWithEmailAndPassword(values.email, values.password)
+    .then((userCredential) => {
+      firebase.auth().currentUser.sendEmailVerification()
+        .then(() => dispatch({ type: types.SIGN_UP_GUCCI, userCredential }))
+        .catch(error => dispatch({ type: types.SIGN_UP_FAIL, error }));
+    }).catch((error) => {
+      dispatch({ type: types.SIGN_UP_FAIL, error });
+    });
+};
 
 export const login = values => (dispatch) => {
   firebase.auth().signInWithEmailAndPassword(values.email, values.password)
