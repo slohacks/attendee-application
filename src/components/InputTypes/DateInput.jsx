fe457bf9ba9cc76c5ec086c3e3b51991fd9d4694
stonewallstan <stanleyarmstrong31@gmail.com>
2018-11-12 20:48:07 -0800

    feat(src): add date picker for graduation date

---
+++ src/components/InputTypes/DateInput.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DateInput = ({
+  input,
+  label,
+  disabled,
+  meta: { touched, error },
+}) => (
+  <div>
+    <label htmlFor="value">
+      {label}
+      <input type="date" {...input} disabled={disabled} />
+    </label>
+    <div className="error-message">
+      {touched ? error : ''}
+    </div>
+  </div>
+);
+
+
+DateInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  input: PropTypes.shape({}).isRequired,
+  disabled: PropTypes.bool.isRequired,
+  meta: PropTypes.shape({
+    touched: PropTypes.bool,
+    error: PropTypes.string,
+  }).isRequired,
+};
+
+export default DateInput;
