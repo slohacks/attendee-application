e0876217a185d69c05f88fbacec345826aef0f1a
Mian Uddin <mianuddin@gmail.com>
2018-11-30 22:47:55 -0800

    fix(index.jsx): import HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import ReduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
