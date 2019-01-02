f56b6c5cfed0292780eb399da761bd3b7f220f2c
Jumer-Caragay <jumer.caragay@gmail.com>
2019-01-02 02:01:01 -0800

    feat(SelectInput.jsx): add support for key values in select component

--- src/components/InputTypes/SelectInput.jsx
+++ src/components/InputTypes/SelectInput.jsx
@@ -11,7 +11,7 @@ class SelectInput extends Component {
     const { options } = this.props;
     return Object.keys(options).map((option) => {
       return (
-        <MenuItem key={option} value={option}>
+        <MenuItem key={option} value={options[option]}>
           {option}
         </MenuItem>
       );
