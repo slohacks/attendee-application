3f5fa8be2999d81f39967d425c42aa01300a7fbd
Tommy Youn <toyoun@calpoly.edu>
2019-09-29 16:10:55 -0700

    feat(index.jsx): add questionnaire route

--- src/index.jsx
+++ src/index.jsx
@@ -28,6 +28,7 @@ ReactDOM.render(
         <Route path="/forgotpassword" component={ForgotPassword} />
         <Route path="/confirmation" component={Confirmation} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/questionnaire/:id" component={Questionnaire} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route component={NotFound} />
