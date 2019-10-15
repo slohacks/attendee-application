bd382e52f868113ea6ea85766247101ae0089aee
Darryl Yeo <dev@darryl-yeo.com>
2019-10-15 04:10:21 +0000

    refactor(actions/: lint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,128 +1,128 @@
-import axios from "axios";
-import * as types from "./types";
+import axios from 'axios';
+import * as types from './types';
 
-const API_PATH = "https://slohacks-backend-api.herokuapp.com";
+const API_PATH = 'https://slohacks-backend-api.herokuapp.com';
 
-export const signUp = (values, callback) => dispatch => {
+export const signUp = (values, callback) => (dispatch) => {
   dispatch({ type: types.SIGN_UP_ATTEMPT });
   axios
     .post(`${API_PATH}/users/signup`, {
       email: values.email,
-      password: values.password
+      password: values.password,
     })
-    .then(response => {
+    .then((response) => {
       const userCredential = response.data;
       dispatch({ type: types.SIGN_UP_GUCCI, userCredential });
       callback();
     })
-    .catch(error => {
+    .catch((error) => {
       const { errorMessage } = error.response.data;
       dispatch({ type: types.SIGN_UP_FAIL, errorMessage });
     });
 };
 
-export const login = values => dispatch => {
+export const login = values => (dispatch) => {
   dispatch({ type: types.LOGIN_ATTEMPT });
   axios
     .post(`${API_PATH}/users/login`, {
       email: values.email,
-      password: values.password
+      password: values.password,
     })
-    .then(response => {
+    .then((response) => {
       const userCredential = response.data;
       dispatch({ type: types.LOGIN_GUCCI, userCredential });
     })
-    .catch(error => {
+    .catch((error) => {
       const { errorMessage } = error.response.data;
       dispatch({ type: types.LOGIN_FAIL, errorMessage });
     });
 };
 
-export const rsvpResponse = () => dispatch => {
-    dispatch({ type: types.UPDATE_RSVP, rsvpVal: true });
+export const rsvpResponse = () => (dispatch) => {
+  dispatch({ type: types.UPDATE_RSVP, rsvpVal: true });
 };
 
-export const forgotPassword = values => dispatch => {
+export const forgotPassword = values => (dispatch) => {
   dispatch({ type: types.FORGOT_PASS_ATTEMPT });
   axios
     .post(`${API_PATH}/forgot-password/request`, {
-      email: values.email
+      email: values.email,
     })
-    .then(response => {
+    .then((response) => {
       const { success } = response.data;
       if (success === true) {
         dispatch({ type: types.FORGOT_PASS_GUCCI });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       const { errorMessage } = error.response.data;
       dispatch({ type: types.FORGOT_PASS_FAIL, errorMessage });
     });
 };
 
-export const signout = () => dispatch => {
+export const signout = () => (dispatch) => {
   dispatch({ type: types.SIGN_OUT_GUCCI });
 };
 
 export function submitResponse(formProps) {
   return {
     type: types.SAVE_RESPONSE,
-    payload: formProps
+    payload: formProps,
   };
 }
 
-export const uploadResume = (user, resume, onChange) => dispatch => {
+export const uploadResume = (user, resume, onChange) => (dispatch) => {
   dispatch({ type: types.UPLOAD_RESUME_ATTEMPT });
   const data = new FormData();
-  data.append("resume", resume);
+  data.append('resume', resume);
 
   axios
     .post(`${API_PATH}/resumes`, data, {
       headers: {
-        Authorization: user.token
-      }
+        Authorization: user.token,
+      },
     })
     .then(() => {
       dispatch({ type: types.UPLOAD_RESUME_GUCCI, resume });
       onChange(resume.name);
     })
-    .catch(error => {
+    .catch((error) => {
       const { errorMessage } = error.response.data;
       dispatch({
         type: types.UPLOAD_RESUME_FAIL,
-        error: errorMessage
+        error: errorMessage,
       });
     });
 };
 
 export const clearResume = () => {
   return {
-    type: types.CLEAR_RESUME_ERROR
+    type: types.CLEAR_RESUME_ERROR,
   };
 };
 
-export const submitApp = (user, form) => dispatch => {
+export const submitApp = (user, form) => (dispatch) => {
   const formCopy = form;
   dispatch({ type: types.ATTEMPT_SUBMISSION });
   axios
     .post(`${API_PATH}/applications`, formCopy, {
       headers: {
-        Authorization: user.token
-      }
+        Authorization: user.token,
+      },
     })
-    .then(response => {
+    .then((response) => {
       dispatch({
         type: types.UPDATE_APPLICATION_TRUE,
         app: response.data,
-        rsvpInv: false
+        rsvpInv: false,
       });
       dispatch({ type: types.SUBMISSION_GUCCI });
     })
-    .catch(error => {
+    .catch((error) => {
       const { errorMessage } = error.response.data;
       dispatch({
         type: types.SUBMISSION_FAIL,
-        error: errorMessage
+        error: errorMessage,
       });
     });
 };
